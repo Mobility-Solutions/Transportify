b437a7ad35efa98b7a10f7127f662118cc4d0d7f
Andres <andresen1998@hotmail.com>
2019-10-12 12:35:31 +0200

    Editado los modelos para que guarden la id del objeto al que referencian y que tengan un constructor desde una Snapshot

--- lib/modelos/Paquete.dart
+++ lib/modelos/Paquete.dart
@@ -1,13 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'PuntoTransportify.dart';
 import 'Usuario.dart';
 
 class Paquete {
+  String id;
   double largo, ancho, alto;
   double peso;
   bool fragil;
-  PuntoTransportify destino;
-  PuntoTransportify origen;
-  Usuario remitente;
+  String destinoId;
+  String origenId;
+  String remitenteId;
   double precio;
 
   Paquete(
@@ -16,8 +19,24 @@ class Paquete {
       this.largo,
       this.peso,
       this.fragil,
-      this.destino,
-      this.origen,
-      this.remitente,
+      this.destinoId,
+      this.origenId,
+      this.remitenteId,
       this.precio});
+
+  Paquete.fromSnapshot(DocumentSnapshot snapshot) {
+    this.id = snapshot.documentID;
+    this.largo = snapshot['largo'];
+    this.ancho = snapshot['ancho'];
+    this.alto = snapshot['alto'];
+    this.peso = snapshot['peso'];
+    this.fragil = snapshot['fragil'];
+    this.precio = snapshot['precio'];
+
+    this.destinoId = snapshot['id_destino'];
+    this.origenId = snapshot['id_origen'];
+    this.remitenteId = snapshot['id_remitente'];
+  }
 }
+
+
--- lib/modelos/Usuario.dart
+++ lib/modelos/Usuario.dart
@@ -1,5 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Usuario{
   String nombre;
+  String id;
 
   Usuario({this.nombre});
+
+  Usuario.fromSnapshot(DocumentSnapshot snapshot) {
+    this.id = snapshot.documentID;
+    this.nombre = snapshot['nombre'];
+  }
 }
