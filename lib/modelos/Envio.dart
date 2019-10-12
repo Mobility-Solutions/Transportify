e8779b16d98a8ba423d33a196b8582d1f4d9c6bf
Andres <andresen1998@hotmail.com>
2019-10-12 12:35:31 +0200

    Editado los modelos para que guarden la id del objeto al que referencian y que tengan un constructor desde una Snapshot

--- lib/modelos/Envio.dart
+++ lib/modelos/Envio.dart
@@ -1,12 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'Paquete.dart';
 import 'Viaje.dart';
 import 'enumerados/EstadoPaquete.dart';
 
 class Envio {
+  String id;
   EstadoPaquete estado;
-  Paquete paquete;
-  Viaje viaje;
+  String paqueteId;
+  String viajeId;
 
-  Envio({this.estado, this.paquete, this.viaje});
+  Envio({this.estado, this.paqueteId, this.viajeId});
+
+  Envio.fromSnapshot(DocumentSnapshot snapshot) {
+    this.id = snapshot.documentID;
+    this.estado = snapshot['estado'];
+    this.paqueteId = snapshot['id_paquete'];
+    this.viajeId = snapshot['id_viaje'];
+  }
 
 }
