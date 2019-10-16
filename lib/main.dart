e7e98a3508efe75b200d14ee5bc41d9c60878f80
Alejandro Gómez <algono@inf.upv.es>
2019-10-16 19:14:05 +0200

    Eliminados archivos WIP del merge de UT 2679

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:transportify/middleware/myWidgets.dart';
 import 'package:transportify/modelos/PuntoTransportify.dart';
 import 'package:transportify/vistas/paqueteForm.dart';
 import 'package:transportify/vistas/paqueteListView.dart';
