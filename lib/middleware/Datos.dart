121396c9d994492102280deb677a20b957705900
Alejandro Gómez <algono@inf.upv.es>
2019-10-17 11:04:00 +0200

    Nuevo metodo para crear nuevas entradas

--- lib/middleware/Datos.dart
+++ lib/middleware/Datos.dart
@@ -3,20 +3,24 @@ import 'package:flutter/widgets.dart';
 
 class Datos {
   static StreamBuilder<QuerySnapshot> obtenerStreamBuilderCollectionBD(
-      String collection,
+      String collectionPath,
       Function(BuildContext, AsyncSnapshot<QuerySnapshot>) builder) {
     return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection(collection).snapshots(),
+      stream: Firestore.instance.collection(collectionPath).snapshots(),
       builder: builder,
     );
   }
 
   static StreamBuilder<DocumentSnapshot> obtenerStreamBuilderDocumentBD(
-      String path,
+      String documentPath,
       Function(BuildContext, AsyncSnapshot<DocumentSnapshot>) builder) {
     return StreamBuilder<DocumentSnapshot>(
-      stream: Firestore.instance.document(path).snapshots(),
+      stream: Firestore.instance.document(documentPath).snapshots(),
       builder: builder,
     );
+  }
+
+  static Future<DocumentReference> crearDocument(String collectionPath, Map<String, dynamic> data) {
+    return Firestore.instance.collection(collectionPath).add(data);
   }
 }
