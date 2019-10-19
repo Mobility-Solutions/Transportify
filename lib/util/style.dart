9709e7bb94f193de488882512d8f7ca8ec639d26
Nacchofer31 <ignaciofer31@gmail.com>
2019-10-19 15:40:23 +0200

    Added new style structure

--- lib/util/style.dart
+++ lib/util/style.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 class TransportifyColors {
   static const String primaryValueString = "#3C6997";
   static const String primaryLightValueString = "#6D97C8";
-  static const String primaryDarkValueString = "#003F69";
+  static const String primaryDarkValueString = "#2A4D70";
 
   static const int primaryValue = 0xff3C6997;
   static const int primaryLightValue = 0xff6D97C8;
-  static const int primaryDarkValue = 0xff003F69;
+  static const int primaryDarkValue = 0xff2a4d70;
 
   static const MaterialColor primarySwatch = const MaterialColor(
     primaryValue,
@@ -30,3 +30,22 @@ class TransportifyColors {
   static const scaffoldBackground = Color(primaryValue);
 
 }
+
+class TransportifyContainer extends StatelessWidget {
+  TransportifyContainer({
+    this.child,
+    this.padding = 15.0,
+    this.color = TransportifyColors.primarySwatch,
+    this.vertical = true
+  });
+
+  final double padding;
+  final Widget child;
+  final Color color;
+  final bool vertical;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(padding: vertical ? EdgeInsets.symmetric(vertical: padding) : EdgeInsets.symmetric(horizontal: padding),
+    child: child);
+  }}
