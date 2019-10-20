d2f7d5a56dd2338cac0b982b191b4662c339ebb2
A.Tauste <taustano23@gmail.com>
2019-10-20 22:34:52 +0200

    Publicar viaje, first final version
    Added a new class:
    -ViajeTransportifyBD.dart
    
    Important files changed:
    -ViajeForm.dart => all changes are based on "PaqueteForm.dart" in order to be consistent with the structure and code of the project.
    
    -Viaje.dart => added cargaMaxima attribute to the class.
    
    -ViajeTransportifyBD.dart => based on "PaqueteTransportifyBD.dart".

--- lib/modelos/Viaje.dart
+++ lib/modelos/Viaje.dart
@@ -5,17 +5,18 @@ import 'PuntoTransportify.dart';
 
 class Viaje {
   String id;
+  double cargaMaxima;
   DateTime fecha;
   String destinoId;
   String origenId;
   String transportistaId;
 
-  Viaje({this.fecha, this.destinoId, this.origenId, this.transportistaId});
+  Viaje({this.cargaMaxima, this.fecha, this.destinoId, this.origenId, this.transportistaId});
 
   Viaje.fromSnapshot(DocumentSnapshot snapshot) {
     this.id = snapshot.documentID;
+    this.cargaMaxima = snapshot['carga_maxima'];
     this.fecha = snapshot['fecha'];
-
     this.destinoId = snapshot['id_destino'];
     this.origenId = snapshot['id_origen'];
     this.transportistaId = snapshot['id_transportista'];
