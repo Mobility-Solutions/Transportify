46d5d42d4569e752767e7eaf1e11d3be0215f6d2
algono <alejandrogomeznoe@gmail.com>
2019-10-22 00:22:13 +0200

    Quitados huecos y código comentado innecesario

--- lib/middleware/EnviosBD.dart
+++ lib/middleware/EnviosBD.dart
@@ -14,31 +14,26 @@ class EnviosBD {
   static String obtenerIdPaquete(DocumentSnapshot snapshot) => snapshot[atributo_id_paquete];
   static String obtenerIdViaje(DocumentSnapshot snapshot) => snapshot[atributo_id_viaje];
 
-
-static Function(BuildContext, AsyncSnapshot<QuerySnapshot>) _obtenerListaEnviosBuilder(Function(int estado) onTapMethod) {
-    Function(BuildContext, AsyncSnapshot<dynamic>) listaEnviosBuilder =  (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-      if (!snapshot.hasData) return const Text('Cargando...');
-      
-      //Metodo constructor del dropdown
-      Widget listView;
-
-     listView = new ListView(
-        children: snapshot.data.documents.map<Widget>((document) {
-          return new ListTile(           
-          title: new Text("Id paquete: " + document['id_paquete']),
-          onTap: ()=>onTapMethod(document['estado']),
-          );
-        }).toList(),
+  static Function(BuildContext, AsyncSnapshot<QuerySnapshot>) _obtenerListaEnviosBuilder(Function(int estado) onTapMethod) {
+      Function(BuildContext, AsyncSnapshot<dynamic>) listaEnviosBuilder =  (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (!snapshot.hasData) return const Text('Cargando...');
         
-      );
-      
-      
-      
+        //Metodo constructor del dropdown
+        Widget listView;
 
+        listView = new ListView(
+            children: snapshot.data.documents.map<Widget>((document) {
+              return new ListTile(           
+              title: new Text("Id paquete: " + document['id_paquete']),
+              onTap: ()=>onTapMethod(document['estado']),
+              );
+            }).toList(),
+        );
+          
+        return listView;
+      };
 
-      return listView;
-    };
-    return listaEnviosBuilder;
+      return listaEnviosBuilder;
   }
 
   static Widget obtenerListaEnvios(Function(int estado) onTapMethod){
@@ -46,165 +41,3 @@ static Function(BuildContext, AsyncSnapshot<QuerySnapshot>) _obtenerListaEnviosB
     return Datos.obtenerStreamBuilderCollectionBD(coleccion_envios, builder);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-  static StreamBuilder<QuerySnapshot> obtenerStreamBuilderListado(
-      Function(BuildContext, AsyncSnapshot<QuerySnapshot>) builder) {
-    return Datos.obtenerStreamBuilderCollectionBD(coleccion_envios, builder);
-  }
-
-  static Widget obtenerDropDownCiudadesYListadoPuntos(
-      {Function(String) onCiudadChanged,
-      String ciudadValue,
-      Function(PuntoTransportify) onPuntoChanged}) {
-    return obtenerStreamBuilderListado(
-        _obtenerDropDownBuilder(onCiudadChanged, ciudadValue, onPuntoChanged));
-  }
-
-  static Function(BuildContext, AsyncSnapshot<QuerySnapshot>)
-      _obtenerDropDownBuilder(Function(String) onCiudadChanged,
-          String ciudadValue, Function(PuntoTransportify) onPuntoChanged) {
-    return (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-      return _obtenerDropDownCiudadesYListadoPuntos(
-          context, snapshot, onCiudadChanged, ciudadValue, onPuntoChanged);
-    };
-  }
-
-  static Widget _obtenerDropDownCiudadesYListadoPuntos(
-      BuildContext context,
-      AsyncSnapshot<QuerySnapshot> snapshot,
-      Function(String) onCiudadChanged,
-      String ciudadSeleccionada,
-      Function(PuntoTransportify) onPuntoChanged) {
-    if (!snapshot.hasData) return const Text('Cargando...');
-
-    List<DropdownMenuItem<String>> items = [];
-    var ciudades =
-        snapshot.data.documents.map((doc) => doc[atributo_id_viaje]).toSet();
-
-    for (String ciudad in ciudades) {
-      if (ciudad != null && ciudad.length > 0) {
-        items.add(
-          DropdownMenuItem<String>(
-            child: Text(ciudad),
-            value: ciudad,
-          ),
-        );
-      }
-    }
-
-    var puntosDeLaCiudad = _obtenerPuntosDeLaCiudad(
-      puntos: snapshot.data.documents,
-      ciudad: ciudadSeleccionada,
-    );
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        DropdownButton<String>(
-          items: items,
-          onChanged: onCiudadChanged,
-          value: ciudadSeleccionada,
-        ),
-        SizedBox(
-          height: 10.0,
-        ),
-        Expanded(
-          child: SizedBox(
-            height: 200.0,
-            child: ListView.builder(
-              itemExtent: 30.0,
-              itemCount: ciudades.length,
-              itemBuilder: (context, index) {
-                if (index >= 0 && index < puntosDeLaCiudad.length) {
-                  var punto = puntosDeLaCiudad.elementAt(index);
-                  return _obtenerListViewItem(punto, onPuntoChanged);
-                } else {
-                  return null;
-                }
-              },
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  static Iterable<DocumentSnapshot> _obtenerPuntosDeLaCiudad(
-      {Iterable<DocumentSnapshot> puntos, String ciudad}) {
-    return puntos.where((punto) => punto[atributo_id_viaje] == ciudad);
-  }
-
-  static Widget _obtenerListViewItem(
-      DocumentSnapshot snapshot, Function(PuntoTransportify) onSelected) {
-    PuntoTransportify punto = PuntoTransportify.fromSnapshot(snapshot);
-
-    String texto = punto?.nombre;
-    if (texto == null || texto.length == 0) {
-      texto = punto?.direccion ?? "<punto-sin-nombre>";
-    }
-
-    Function onTap;
-    if (onSelected != null) {
-      onTap = () => onSelected(punto);
-    }
-
-    return ListTile(
-      title: Text(texto),
-      onTap: onTap,
-    );
-  }
-}
-*/
