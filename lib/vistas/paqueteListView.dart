e4931330d013011655e572dd75eb3b05bcad67c3
vinxi15 <vinxi115@gmail.com>
2019-10-21 18:40:08 +0200

    buscar paquete con algunos defectos

--- lib/vistas/paqueteListView.dart
+++ lib/vistas/paqueteListView.dart
@@ -1,101 +1,227 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:transportify/middleware/PuntoTransportifyBD.dart';
 import 'package:transportify/util/style.dart';
+import 'package:transportify/middleware/Datos.dart';
 import 'package:transportify/modelos/Paquete.dart';
+import 'package:transportify/modelos/PuntoTransportify.dart';
 
-class PaqueteListView extends StatelessWidget {
+class PaqueteListView extends StatefulWidget {
+
+  @override
+  _PaqueteListViewState createState() => _PaqueteListViewState();
+}
+
+class _PaqueteListViewState extends State<PaqueteListView>{
+
+  static bool pressed = false;
+  static int paquetesEncontrados = 0;
+  String _ciudadOrigenSeleccionada;
+  String _ciudadDestinoSeleccionada;
+  static String idPuntoOrigen = '';
+  static String idPuntoDestino = '';
+  static List<DocumentSnapshot> listaPaquetes = List<DocumentSnapshot>();
+  
+  //paquetesEncontrados = listaPaquetes.length;
+  
   @override
   Widget build(BuildContext context) {
 
-    //final List<Paquete> listaPaquetes = ;
+   
+    
 
     
     return Scaffold(
       appBar: AppBar(
         title: Text("Buscar Paquete"),
       ),
-      body: puntosOrigenDestino,
+      body: 
+        Column(
+          children:[
+            
+            Text(
+              'Punto Origen',
+              textAlign: TextAlign.center,
+              style: TextStyle (fontSize: 20),
+            ),
+
+            Expanded(
+              child:
+                obtenerDropDownCiudadesYListadoPuntos(
+                  onCiudadChanged: (nuevaCiudad) {
+                    setState(() {
+                      this._ciudadOrigenSeleccionada = nuevaCiudad;
+                      idPuntoOrigen = _ciudadOrigenSeleccionada;
+                    });
+                  },
+                  puntoSeleccionado: _ciudadOrigenSeleccionada,
+                ),
+            ),
+
+            Text(
+              'Punto Destino',
+              textAlign: TextAlign.center,
+              style: TextStyle (fontSize: 20),
+            ),
+
+            
+              
+            Expanded(
+              child:
+                obtenerDropDownCiudadesYListadoPuntos(
+                  onCiudadChanged: (nuevaCiudad) {
+                    setState(() {
+                      this._ciudadDestinoSeleccionada = nuevaCiudad;
+                      idPuntoDestino = _ciudadDestinoSeleccionada;
+                      print(_ciudadDestinoSeleccionada);
+                    });
+                  },
+                  puntoSeleccionado: _ciudadDestinoSeleccionada,
+                ),
+            ),
+            resultadosBusqueda,
+            new Expanded(
+              child: sacarListaPaquetes(),
+            )      
+          ],
+        ),
       backgroundColor: TransportifyColors.primarySwatch,
     );
   }
-  Widget puntosOrigenDestino = Container(
-    padding: const EdgeInsets.all(32),
-    child: Column(
-      children: [
-        
-        TextFormField(
-          decoration: InputDecoration(
-            hintText: 'Punto Origen',
-            filled: true,
-            fillColor: Colors.white,
-            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
-          ),
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-            hintText: 'Punto Destino',
-            filled: true,
-            fillColor: Colors.white,
-            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
-          ),
-        ),
-        
-      ]
-    ),
-  );
 
   
-  Widget _sacarListaPaquetes(){
+
+  static Widget resultadosBusqueda = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children:[
+        Container(
+          margin: const EdgeInsets.all(10),
+          child: 
+            Text(
+              'Paquetes encontrados:  ${paquetesEncontrados}',
+              style: TextStyle ( color : Colors.white, fontSize: 20),
+            ),
+          
+        ),
+          Container(
+            decoration: new BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.blue,
+            ),
+            
+            child:
+            IconButton(
+              icon: Icon(Icons.search),
+              color: Colors.white,
+              onPressed: (){
+                
+                print('botón pulsado');
+                
+              },
+            ),
+          ),
+          ],
+        ),
+    );
+  
+
+  static StreamBuilder<QuerySnapshot> obtenerStreamBuilderListado(
+      Function(BuildContext, AsyncSnapshot<QuerySnapshot>) builder) {
+      return Datos.obtenerStreamBuilderCollectionBD('paquetes', builder);
+    }
+  
+  static Widget sacarListaPaquetes () {
+    return obtenerStreamBuilderListado(obtenerListaBuilder());
+  }
+
+  static Function (BuildContext, AsyncSnapshot<QuerySnapshot>)
+    obtenerListaBuilder(){
+      return (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+      return _sacarListaPaquetes(context,snapshot);
+    };
+  }
+
+  static Widget _sacarListaPaquetes(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+    if (!snapshot.hasData) return const Text('Cargando...');
+    List<DocumentSnapshot> coleccion = snapshot.data.documents;
+
+    for (int i = 0; i<coleccion.length;i++){
+      if(coleccion[i]['id_destino'].toString() == idPuntoDestino && coleccion[i]['id_origen'].toString() == idPuntoOrigen){
+        listaPaquetes.add(coleccion[i]);
+      }
+    }
+
     return ListView.separated(
-          padding : const EdgeInsets.all(8),
-          itemCount : 5,
+          padding : const EdgeInsets.only(left: 20,right: 20,top: 10,bottom: 10),
+          itemCount : listaPaquetes.length,
           itemBuilder : (BuildContext context, int index) {
             return InkWell(
-              child:
+              child: 
+              Center(
+                child:
                 Container(
-                  height: 50,
-                  color: Colors.amber[0],
-                  child: Row(
+                  decoration: new BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                  ),
+                  margin: const EdgeInsets.all(5),
+                  height: 70,
+                  child: Row(         
                     children: [
+                      Expanded(
+                        child:
                       Column(
+                        
                         children: [
                           Text(
-                            'Precio: ',
-                            style: TextStyle ( color : Colors.grey[500]),
+                            'Precio: ${listaPaquetes[index]['Precio']}',
+                            textAlign: TextAlign.right,
+                            style: TextStyle ( color : Colors.grey[500], fontSize: 20),
+                            
                           ),
                           Text(
-                            'Peso: ',
-                            style: TextStyle ( color : Colors.grey[500]),
+                            'Peso: ${listaPaquetes[index]['peso']}',
+                            textAlign: TextAlign.left,
+                            style: TextStyle ( color : Colors.grey[500], fontSize: 20),
                           ),
                           Text(
-                            'Fragilidad: ',
-                            style: TextStyle ( color : Colors.grey[500]),
+                            'Fragilidad: ${listaPaquetes[index]['fragil']}',
+                            textAlign: TextAlign.left,
+                            style: TextStyle ( color : Colors.grey[500], fontSize: 20),
                           ),
                           
                         ],
                       ),
+                      ),
+                      Expanded(
+                        child:
                       Column(
                         children: [
                           Text(
-                            'Alto: ',
-                            style: TextStyle ( color : Colors.grey[500]),
+                            'Alto: ${listaPaquetes[index]['alto']}',
+                            style: TextStyle ( color : Colors.grey[500], fontSize: 20),
                           ),
                           Text(
-                            'Ancho: ',
-                            style: TextStyle ( color : Colors.grey[500]),
+                            'Ancho: ${listaPaquetes[index]['ancho']}',
+                            style: TextStyle ( color : Colors.grey[500], fontSize: 20),
                           ),
                           Text(
-                            'Largo: ',
-                            style : TextStyle ( color : Colors.grey[500]),
+                            'Largo: ${listaPaquetes[index]['largo']}',
+                            style: TextStyle ( color : Colors.grey[500], fontSize: 20),
                           ),
                           
                         ],
+                      ),
                       ),
                     ],
                   ),
                 ),
+              ),
                 onTap: () {
                   print('Has pulsado un paquete');
                 },
@@ -104,4 +230,72 @@ class PaqueteListView extends StatelessWidget {
           separatorBuilder: (BuildContext context, int index) => const Divider(),
         );
   }
+
+  static StreamBuilder<QuerySnapshot> obtenerStreamBuilderListadoPuntos(
+      Function(BuildContext, AsyncSnapshot<QuerySnapshot>) builder) {
+    return Datos.obtenerStreamBuilderCollectionBD('puntos_transportify', builder);
+  }
+
+  static Widget obtenerDropDownCiudadesYListadoPuntos(
+      {Function(String) onCiudadChanged,
+      String puntoSeleccionado,
+      Function(PuntoTransportify) onPuntoChanged}) {
+    return obtenerStreamBuilderListadoPuntos(
+        _obtenerDropDownBuilder(onCiudadChanged, puntoSeleccionado, onPuntoChanged));
+  }
+
+  static Function(BuildContext, AsyncSnapshot<QuerySnapshot>)
+      _obtenerDropDownBuilder(Function(String) onCiudadChanged,
+          String puntoSeleccionado, Function(PuntoTransportify) onPuntoChanged) {
+    return (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+      return _obtenerDropDownCiudadesYListadoPuntos(
+          context, snapshot, onCiudadChanged, puntoSeleccionado, onPuntoChanged);
+    };
+  }
+
+  static Widget _obtenerDropDownCiudadesYListadoPuntos(
+      BuildContext context,
+      AsyncSnapshot<QuerySnapshot> snapshot,
+      Function(String) onCiudadChanged,
+      String puntoSeleccionado,
+      Function(PuntoTransportify) onPuntoChanged) {
+    if (!snapshot.hasData) return const Text('Cargando...');
+
+    List<DropdownMenuItem<String>> items = [];
+        List<DocumentSnapshot> coleccion = snapshot.data.documents;
+
+    
+    for (int i = 0; i<coleccion.length; i++) {
+      if(coleccion[i]['direccion'] != null)
+        items.add(
+          DropdownMenuItem<String>(
+            child: Text(coleccion[i]['ciudad']+', '+coleccion[i]['direccion']),
+            value: coleccion[i].documentID,
+          ),
+        );
+    }
+
+    
+
+    return Column(
+      children: [
+        DropdownButton<String>(
+          isExpanded: false,
+          iconSize: 20,
+          items: items,
+          onChanged: onCiudadChanged,
+          value: puntoSeleccionado,
+          hint: Text('Selecciona un Punto'),
+        ),
+        SizedBox(height: 50)
+      ],
+        
+        
+      
+      
+    );
+  }
+
+
+
 }
