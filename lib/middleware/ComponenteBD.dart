39bdad27703b495e222a2016f8e9dd8f0967dadf
algono <alejandrogomeznoe@gmail.com>
2019-10-25 21:25:46 +0200

    Añadido sistema creación en BD

--- lib/middleware/ComponenteBD.dart
+++ lib/middleware/ComponenteBD.dart
@@ -1,13 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class ComponenteBD {
-  DocumentReference reference;
+  final CollectionReference coleccion;
+
+  DocumentReference _reference;
+
+  DocumentReference get reference => _reference;
   
   String get id => reference?.documentID;
 
-  ComponenteBD({this.reference});
+  ComponenteBD({String coleccion})
+  : this.coleccion = Firestore.instance.collection(coleccion);
 
-  ComponenteBD.fromSnapshot(DocumentSnapshot snapshot) {
+  ComponenteBD.fromReference({DocumentReference reference}) :
+    this._reference = reference,
+    this.coleccion = reference.parent();
+
+  ComponenteBD.fromSnapshot(DocumentSnapshot snapshot)
+  : this.coleccion = snapshot.reference.parent() {
     this.loadFromSnapshot(snapshot);
   }
 
@@ -18,21 +28,36 @@ abstract class ComponenteBD {
   int get hashCode => id.hashCode;
 
   void loadFromSnapshot(DocumentSnapshot snapshot) {
-    this.reference = snapshot.reference;
+    this._reference = snapshot.reference;
   }
 
   Map<String, dynamic> toMap();
 
+  Future<void> crearEnBD() {
+    Map<String, dynamic> map = this.toMap();
+    if (map != null) {
+      if (reference == null) {
+        return coleccion.add(map).then((reference) {
+          this._reference = reference;
+        });
+      } else {
+        return reference.setData(map);
+      }
+    } else {
+      throw UnsupportedError("No se pueden crear instancias de este objeto");
+    }
+  }
+
   Future<void> updateBD() {
     Map<String, dynamic> map = this.toMap();
     if (map != null) {
-      return reference.updateData(map);
+      return reference?.updateData(map);
     } else {
       throw UnsupportedError("Este objeto es de solo lectura");
     }
   }
 
-  Future<void> revertToBD() => reference.get().then((snapshot) => loadFromSnapshot(snapshot));
+  Future<void> revertToBD() => reference?.get()?.then((snapshot) => loadFromSnapshot(snapshot));
 
-  Future<void> deleteFromBD() => reference.delete();
+  Future<void> deleteFromBD() => reference?.delete();
 }
