c795592f80a42ac5ab1ca44b7fd229cb9dac3d8c
algono <alejandrogomeznoe@gmail.com>
2019-11-02 18:10:38 +0100

    Adaptación seguimiento envios a nuevo middleware

--- lib/middleware/EnviosBD.dart
+++ lib/middleware/EnviosBD.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:transportify/middleware/Datos.dart';
+import 'package:transportify/modelos/Envio.dart';
 import 'package:transportify/modelos/enumerados/EstadoPaquete.dart';
 
 class EnviosBD {
@@ -11,33 +12,48 @@ class EnviosBD {
   static const String atributo_paquete = 'paquete';
   static const String atributo_viaje = 'viaje';
 
-  static EstadoPaquete obtenerEstado(DocumentSnapshot snapshot) => snapshot[atributo_estado];
-  static DocumentReference obtenerPaquete(DocumentSnapshot snapshot) => snapshot[atributo_paquete];
-  static DocumentReference obtenerViaje(DocumentSnapshot snapshot) => snapshot[atributo_viaje];
+  static EstadoPaquete obtenerEstado(DocumentSnapshot snapshot) =>
+      EstadoPaquete.values[snapshot[atributo_estado]];
+  static DocumentReference obtenerPaquete(DocumentSnapshot snapshot) =>
+      snapshot[atributo_paquete];
+  static DocumentReference obtenerViaje(DocumentSnapshot snapshot) =>
+      snapshot[atributo_viaje];
 
-  static Function(BuildContext, AsyncSnapshot<QuerySnapshot>) _obtenerListaEnviosBuilder(Function(int estado) onTapMethod) {
-      Function(BuildContext, AsyncSnapshot<dynamic>) listaEnviosBuilder =  (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-        if (!snapshot.hasData) return const Text('Cargando...');
-        
-        //Metodo constructor del dropdown
-        Widget listView;
+  static Function(BuildContext, AsyncSnapshot<QuerySnapshot>)
+      _obtenerListaEnviosBuilder(Function(int estado) onTapMethod) =>
+          (context, snapshot) =>
+              _obtenerListaEnvios(context, snapshot, onTapMethod);
 
-        listView = new ListView(
-            children: snapshot.data.documents.map<Widget>((document) {
-              return new ListTile(           
-              title: new Text("Id paquete: " + document['id_paquete']),
-              onTap: ()=>onTapMethod(document['estado']),
-              );
-            }).toList(),
-        );
-          
-        return listView;
-      };
+  static Widget _obtenerListaEnvios(BuildContext context,
+      AsyncSnapshot<dynamic> snapshot, onTapMethod(int estado)) {
+    if (!snapshot.hasData) return const Text('Cargando...');
 
-      return listaEnviosBuilder;
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        List<DocumentSnapshot> documents = snapshot.data.documents;
+        if (index >= 0 && index < documents.length) {
+          Envio envio = Envio.fromSnapshot(documents[index]);
+          return FutureBuilder(
+            future: envio.waitForInit(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return ListTile(
+                  title: Text("Nombre paquete: " + envio.paquete.nombre),
+                  onTap: () => onTapMethod(envio.estado.index),
+                );
+              } else {
+                return Container();
+              }
+            },
+          );
+        } else {
+          return null;
+        }
+      },
+    );
   }
 
-  static Widget obtenerListaEnvios(Function(int estado) onTapMethod){
+  static Widget obtenerListaEnvios(Function(int estado) onTapMethod) {
     var builder = _obtenerListaEnviosBuilder(onTapMethod);
     return Datos.obtenerStreamBuilderCollectionBD(coleccion_envios, builder);
   }
