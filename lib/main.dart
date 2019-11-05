eec76d35a6e7adaca1c492652408ad5dbc614d33
Nacchofer31 <ignaciofer31@gmail.com>
2019-11-05 19:17:42 +0100

    Added TopPart and new color on styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:transportify/util/style.dart';
 import 'package:transportify/vistas/creacion/CreacionPaqueteForm.dart';
 import 'package:transportify/vistas/busqueda/BusquedaPaqueteForm.dart';
 import 'package:transportify/vistas/seguimiento/SeguimientoForm.dart';
@@ -12,125 +13,136 @@ void main() async =>
     await initializeDateFormatting("es_ES", null).then((_) => runApp(MyApp()));
 
 class MyApp extends StatelessWidget {
-  static const String title = 'Transportify';
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: title,
-      theme: ThemeData(
-        fontFamily: 'Quicksand',
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: title),
+      theme: ThemeData(fontFamily: 'Quicksand'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _ciudadSeleccionada;
-
-  void _onItemTapped(int index) {
-    switch (index) {
-      case 0:
-        Navigator.of(context)
-            .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-          return new CreacionPaqueteForm();
-        }));
-        break;
-      case 1:
-        Navigator.of(context)
-            .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-          return new CreacionViajeForm();
-        }));
-        break;
-      case 2:
-        Navigator.of(context)
-            .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-          return new SeguimientoForm();
-        }));
-        break;
-      case 3:
-        Navigator.of(context)
-            .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-          return new BusquedaPaqueteForm();
-        }));
-        break;
-      case 4:
-        Navigator.of(context)
-            .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-          return new BusquedaViajeForm();
-        }));
-        break;
-      default:
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    int _currentIndex = 0;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: PuntoTransportifyBD.obtenerDropDownCiudadesYListadoPuntos(
-          onCiudadChanged: (nuevaCiudad) {
-            setState(() {
-              this._ciudadSeleccionada = nuevaCiudad;
-            });
-          },
-          ciudadValue: _ciudadSeleccionada,
+      backgroundColor: TransportifyColors.homeBackgroundSwatch,
+      body: Column(
+        children: <Widget>[
+            TopPart()
+        ]
+      )
+    );
+ // This trailing comma makes auto-formatting nicer for build methods.
+  }
+}
+
+class TopPart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 140.0,
+      color: TransportifyColors.primarySwatch[50],
+      child: Material(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(60.0),
+        ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 50.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: TransportifyColors.primarySwatch[900])),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.account_circle,
+                          color: TransportifyColors.primarySwatch[900],
+                          size: 28.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Text(
+                      "PERFIL",
+                      style: TextStyle(
+                        color: TransportifyColors.primarySwatch[900],
+                          fontWeight: FontWeight.w500, fontSize: 12.0),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.pink)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.trending_up,
+                          size: 28.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Text(
+                      "PAQUETES/VIAJES",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, fontSize: 12.0),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.grey[300])),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.trending_up,
+                          size: 28.0,
+                          color: TransportifyColors.primarySwatch[900],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Text(
+                      "SEGUIMIENTO",
+                      style: TextStyle(
+                          color: TransportifyColors.primarySwatch[900],
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12.0),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.add), title: Text('Paquete')),
-          BottomNavigationBarItem(icon: Icon(Icons.add), title: Text('Viaje')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.time_to_leave), title: Text('Seguimiento')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search), title: Text('Paquete')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search), title: Text('Viaje')),
-        ],
-        type: BottomNavigationBarType.fixed,
-        onTap: (int index) {
-          _onItemTapped(index);
-        },
-        selectedItemColor: Colors.blue[800],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
