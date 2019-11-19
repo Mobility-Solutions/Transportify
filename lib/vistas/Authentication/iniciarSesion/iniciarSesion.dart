2dab948d86d874b65705f83d12ff1f5b276be87c
Nacchofer31 <ignaciofer31@gmail.com>
2019-11-19 19:04:38 +0100

    Added close app in Login

--- lib/vistas/Authentication/iniciarSesion/iniciarSesion.dart
+++ lib/vistas/Authentication/iniciarSesion/iniciarSesion.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:transportify/main.dart';
 import 'package:transportify/util/style.dart';
@@ -35,72 +36,88 @@ class IniciarSesionMain extends StatelessWidget {
 class IniciarSesionView extends StatefulWidget {
   final String title = 'Iniciar sesion';
 
-
-
-
   @override
   _IniciarSesionViewState createState() => new _IniciarSesionViewState();
-
-
 }
 
 class _IniciarSesionViewState extends State<IniciarSesionView> {
-
+  Future<bool> _onWillPop() {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('¿Estás seguro?'),
+            content: new Text('¿Quieres salir de la aplicación?'),
+            actions: <Widget>[
+              new FlatButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: new Text('No'),
+              ),
+              new FlatButton(
+                onPressed: () =>
+                    SystemChannels.platform.invokeMethod('SystemNavigator.pop'),
+                child: new Text('Sí'),
+              ),
+            ],
+          ),
+        ) ??
+        false;
+  }
 
   @override
   Widget build(BuildContext context) {
-
-      return Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          //automaticallyImplyLeading: false,
-          leading: new Container(child: SizedBox(),),
-          title: Text(widget.title),
-          backgroundColor: TransportifyColors.primarySwatch,
-
-        ),
-        body: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Builder(builder: (BuildContext context) {
-              return ListView(
-                children: <Widget>[
-                  Center(
-                    child: Icon(
-                      Icons.directions_car,
-                      color: TransportifyColors.primarySwatch,
-                      size: 120,
-                    ),
-                  ),
-                  EmailPasswordForm(),
-                  //GoogleSignInSection(),
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    alignment: Alignment.center,
-                    child: RaisedButton(
-                      color: TransportifyColors.primarySwatch,
-                      textColor: Colors.white,
-                      disabledColor: Colors.grey,
-                      disabledTextColor: Colors.white,
-                      padding: EdgeInsets.all(8.0),
-                      splashColor: Colors.blueAccent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                      onPressed: () async {
-                        abrirRegistrarse();
-                      },
-                      child: Text(
-                        "Registrarse",
-                        style: TextStyle(fontSize: 20.0),
+    return new WillPopScope(
+        onWillPop: _onWillPop,
+        child: Scaffold(
+          backgroundColor: Colors.grey[200],
+          appBar: AppBar(
+            //automaticallyImplyLeading: false,
+            leading: new Container(
+              child: SizedBox(),
+            ),
+            title: Text(widget.title),
+            backgroundColor: TransportifyColors.primarySwatch,
+          ),
+          body: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Builder(builder: (BuildContext context) {
+                return ListView(
+                  children: <Widget>[
+                    Center(
+                      child: Icon(
+                        Icons.directions_car,
+                        color: TransportifyColors.primarySwatch,
+                        size: 120,
                       ),
                     ),
-                  ),
-                  // _OtherProvidersSignInSection(),
-                ],
-              );
-            })),
-      );
-
+                    EmailPasswordForm(),
+                    //GoogleSignInSection(),
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      alignment: Alignment.center,
+                      child: RaisedButton(
+                        color: TransportifyColors.primarySwatch,
+                        textColor: Colors.white,
+                        disabledColor: Colors.grey,
+                        disabledTextColor: Colors.white,
+                        padding: EdgeInsets.all(8.0),
+                        splashColor: Colors.blueAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                        onPressed: () async {
+                          abrirRegistrarse();
+                        },
+                        child: Text(
+                          "Registrarse",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                    ),
+                    // _OtherProvidersSignInSection(),
+                  ],
+                );
+              })),
+        ));
   }
 
   void abrirRegistrarse() {
@@ -110,9 +127,9 @@ class _IniciarSesionViewState extends State<IniciarSesionView> {
     );
   }
 
-  void checkUserIsLogged(){
+  void checkUserIsLogged() {
     FirebaseAuth _auth = FirebaseAuth.instance;
-    if(_auth.currentUser()!=null){
+    if (_auth.currentUser() != null) {
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => MyApp()),
