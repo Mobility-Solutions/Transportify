01dcc388b6d305a1fcdc926f9d169c0f0842dcac
Nacchofer31 <ignaciofer31@gmail.com>
2019-11-19 23:51:47 +0100

    Added font

--- lib/vistas/Authentication/iniciarSesion/iniciarSesion.dart
+++ lib/vistas/Authentication/iniciarSesion/iniciarSesion.dart
@@ -17,7 +17,9 @@ class IniciarSesionMain extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: title,
+      
       theme: ThemeData(
+        fontFamily: 'Quicksand'),
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -27,8 +29,6 @@ class IniciarSesionMain extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
       home: IniciarSesionView(),
     );
   }
