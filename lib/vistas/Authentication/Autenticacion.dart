e180097a0fe6815f9b28e5598a533a05bc5dc879
Nacchofer31 <ignaciofer31@gmail.com>
2019-11-19 23:39:17 +0100

    Added Inicio Route

--- lib/vistas/Authentication/Autenticacion.dart
+++ lib/vistas/Authentication/Autenticacion.dart
@@ -3,12 +3,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:transportify/main.dart';
+import 'package:transportify/vistas/inicio/Inicio.dart';
 
 class Autenticacion{
   static void userSignInCorrectly(BuildContext context){
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => MyApp()),
+      MaterialPageRoute(builder: (context) => MyHomePage()),
     );
   }
 
