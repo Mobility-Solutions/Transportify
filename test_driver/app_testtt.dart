c7a5b27084b06db27fa44f726110f546207ee610
vinxi15 <vinxi115@gmail.com>
2019-11-27 13:48:05 +0100

    pruebas buscar paquete

--- test_driver/app_testtt.dart
+++ test_driver/app_testtt.dart
@@ -27,39 +27,55 @@ void main() {
       }
     });
 
-    test("Fields empty on init.",() async {
-      expect(await driver.getText(ciudadOrigenFinder),"");
-      expect(await driver.getText(ciudadDestinoFinder),"");
-      expect(await driver.getText(fechaTextFinder),"");
-      expect(await driver.getText(horaTextFinder),"");
+    test('Flutter drive methods demo', () async {
+
+      await driver.tap(find.byValueKey('input_email'));
+      await driver.enterText('alcisande@entrepeneur.org');
+      await driver.tap(find.byValueKey('input_password'));
+      await driver.enterText('sandemetrio',timeout: Duration(seconds: 5));
+      await driver.tap(find.byValueKey('button_login'));
     });
 
     test('BuscarPaquete', () async {
       // Use the `driver.getText` method to verify the counter starts at 0.
+      await driver.waitFor(find.byValueKey('buscar_paquete'));
+      await driver.tap(find.byValueKey('buscar_paquete'));
+
+      await driver.waitFor(ciudadOrigenFinder);
       await driver.tap(ciudadOrigenFinder);
-      await driver.tap(find.byValueKey('ciudad'));
+      //await driver.tap(find.byValueKey('ciudad'));
+      await driver.tap(find.text('Valencia'));
+      //await driver.tap(find.byValueKey('ciudad'));
+      await driver.tap(find.text('OK'));
 
+      await driver.waitFor(ciudadDestinoFinder);
       await driver.tap(ciudadDestinoFinder);
-      await driver.tap(find.byValueKey('ciudad'));
+      //await driver.tap(find.byValueKey('ciudad'));
+      await driver.tap(find.text('Madrid'));
+      await driver.tap(find.text('OK'));
 
-      await driver.tap(fechaTextFinder);
-      await driver.enterText('16/12/19');
+      await driver.waitFor(fechaTextFinder);
+      await driver.scroll(fechaTextFinder,0,0,Duration(seconds: 1));
+      await driver.enterText('21/12/19');
 
-      await driver.tap(horaTextFinder);
-      await driver.enterText('01:00');
+      await driver.waitFor(horaTextFinder);
+      await driver.scroll(horaTextFinder,0,0,Duration(seconds: 1));
+      await driver.enterText('08:50');
 
+      await driver.waitFor(buscarBotonFinder);
       await driver.tap(buscarBotonFinder);
-
-       final listFinder = find.byValueKey('listaPaquetes');
-      final paqueteBuscadoFinder = find.byValueKey("paquete_0_Buscado");
+      /*
+      final listFinder = find.byValueKey('listaViajes');
+      final viajeBuscadoFinder = find.byValueKey("cargaViaje");
       final timeline = await driver.traceAction(() async {
         await driver.scrollUntilVisible(
           listFinder,
-          paqueteBuscadoFinder,
+          viajeBuscadoFinder,
           dyScroll: -300.0,
         );
 
-        expect(await driver.getText(paqueteBuscadoFinder), 'paquete_0_Buscado');
+        expect(await driver.getText(viajeBuscadoFinder), "8.0 kg");
+        
       });
 
 
@@ -67,9 +83,9 @@ void main() {
 
       summary.writeSummaryToFile('scrolling_summary', pretty: true);
 
-      summary.writeTimelineToFile('scrolling_timeline', pretty: true);
-      
+      summary.writeTimelineToFile('scrolling_timeline', pretty: true);*/
     });
+
 
   });
 }
