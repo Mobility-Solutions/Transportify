1fbe5911b219448a909b2ecc6a593319a9e5eb9d
Alejandro Gómez <algono@inf.upv.es>
2019-12-04 10:44:35 +0100

    Streams para authStatus y usuario en RootPage

--- lib/vistas/Authentication/iniciarSesion/RootPage.dart
+++ lib/vistas/Authentication/iniciarSesion/RootPage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:transportify/middleware/UsuarioBD.dart';
@@ -7,54 +9,70 @@ import 'package:transportify/vistas/Authentication/iniciarSesion/iniciarSesion.d
 import 'package:transportify/vistas/inicio/Inicio.dart';
 
 class RootPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => _RootPageState();
 }
 
-class _RootPageState extends State<RootPage>{
-
+class _RootPageState extends State<RootPage> {
   AuthStatus authStatus = AuthStatus.NOT_DETERMINED;
   Usuario _usuario;
 
+  final StreamController<Usuario> _usuarioController;
+  final StreamController<AuthStatus> _authStatusController;
+  Stream<Usuario> get usuarioStream => _usuarioController.stream;
+  Stream<AuthStatus> get authStatusStream => _authStatusController.stream;
+
+  _RootPageState()
+      : _usuarioController = StreamController.broadcast(),
+        _authStatusController = StreamController.broadcast() {
+    usuarioStream.listen((newUsuario) {
+      setState(() {
+        _usuario = newUsuario;
+      });
+    });
+
+    authStatusStream.listen((newAuthStatus) {
+      setState(() {
+        authStatus = newAuthStatus;
+      });
+    });
+  }
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    UsuarioBD.obtenerUsuarioActual().then((usuario){
+    UsuarioBD.obtenerUsuarioActual().then((usuario) {
       setState(() {
         if (usuario != null) {
           _usuario = usuario;
         }
-        authStatus = usuario == null ? AuthStatus.NOT_LOGGED_IN : AuthStatus.LOGGED_IN;
+        authStatus =
+            usuario == null ? AuthStatus.NOT_LOGGED_IN : AuthStatus.LOGGED_IN;
       });
     });
   }
 
-  void loginCallback(){
-    UsuarioBD.obtenerUsuarioActual().then((usuario){
-      setState((){
-        _usuario = usuario;
+  void loginCallback() async {
+    await UsuarioBD.obtenerUsuarioActual().then((usuario) {
+      setState(() {
+        _usuarioController.add(usuario);
+        _authStatusController.add(AuthStatus.LOGGED_IN);
       });
     });
+  }
+
+  void logoutCallback() {
     setState(() {
-      authStatus = AuthStatus.LOGGED_IN;
+      _authStatusController.add(AuthStatus.NOT_LOGGED_IN);
+      _usuarioController.add(null);
     });
   }
 
-  void logoutCallback(){
-    setState(() {
-      authStatus = AuthStatus.NOT_LOGGED_IN;
-      _usuario = null;
-    });
-  }
-
-  Widget buildWaitingScreen(){
-    ///TODO: Pantalla de Carga
+  Widget buildWaitingScreen() {
+    ///TODO: Pantalla de carga (splash)
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child:
-        const CircularProgressIndicator(),
+      body: Center(
+        child: const CircularProgressIndicator(),
       ),
     );
   }
@@ -83,4 +101,11 @@ class _RootPageState extends State<RootPage>{
         return buildWaitingScreen();
     }
   }
+
+  @override
+  void dispose() {
+    _usuarioController.close();
+    _authStatusController.close();
+    super.dispose();
   }
+}
