ef49db04bd06482ee8f3aef6007e4923cfb051cf
Nacchofer31 <ignaciofer31@gmail.com>
2019-12-08 22:57:37 +0100

    First call in MiActividadBusqueda

--- lib/vistas/actividad/MiActividadBusqueda.dart
+++ lib/vistas/actividad/MiActividadBusqueda.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:transportify/middleware/ActividadBD.dart';
+import 'package:transportify/middleware/Datos.dart';
+import 'package:transportify/middleware/PaqueteBD.dart';
+import 'package:transportify/middleware/ViajeBD.dart';
 import 'package:transportify/modelos/Paquete.dart';
 import 'package:transportify/modelos/Usuario.dart';
 import 'package:transportify/modelos/Viaje.dart';
 import 'package:transportify/modelos/enumerados/EstadoActividad.dart';
+import '../../middleware/MultipleCollectionStreamSystem.dart';
 
 class MiActividadBusqueda extends StatefulWidget {
   final EstadoActividad estado;
@@ -17,8 +22,8 @@ class MiActividadBusqueda extends StatefulWidget {
 class _MiActividadBusquedaState extends State<MiActividadBusqueda> {
   bool _loading;
   List resultados = new List();
-  Iterable<Paquete> paquetes;
-  Iterable<Viaje> viajes;
+  MultipleCollectionStreamSystem multipleCollection;
+  Map colecciones;
 
   getActividades() async {
     setState(() {
@@ -27,7 +32,9 @@ class _MiActividadBusquedaState extends State<MiActividadBusqueda> {
 
     switch (widget.estado) {
       case EstadoActividad.PUBLICADO:
-        //!TODO llamada middleware de colecciones compartidas.
+        colecciones[Paquete] = PaqueteBD.coleccion_paquetes;
+        colecciones[Viaje] = ViajeBD.coleccion_viajes;
+        multipleCollection = Datos.obtenerStreamsCollectionsBD(colecciones);
         break;
       case EstadoActividad.ENCURSO:
         break;
@@ -47,10 +54,9 @@ class _MiActividadBusquedaState extends State<MiActividadBusqueda> {
     return _loading ?Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).accentColor)),)
     : Column(
             children: <Widget>[
-              //!TODO llamada obtener lista de cards.
+              ActividadBD.obtenerListaActividadesCards(multipleCollectionStreamSystem: multipleCollection,usuario: widget.usuario)
             ],
           );
   }
-
   
 }
