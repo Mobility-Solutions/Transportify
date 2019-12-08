83ddbc6fba8453558e2e96142bb7df94fa109583
Nacchofer31 <ignaciofer31@gmail.com>
2019-12-08 13:08:10 +0100

    Added Expandable list with design

--- lib/vistas/actividad/MiActividadView.dart
+++ lib/vistas/actividad/MiActividadView.dart
@@ -1,56 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:transportify/modelos/Usuario.dart';
+import 'package:transportify/modelos/enumerados/EstadoActividad.dart';
+import 'package:transportify/util/style.dart';
+import 'package:transportify/vistas/actividad/MiActividadBusqueda.dart';
 
 class MiActividadView extends StatefulWidget {
   final Usuario usuario;
 
   MiActividadView(this.usuario) : super();
-  
-    @override
+
+  @override
   _MiActividadViewState createState() => _MiActividadViewState();
 }
 
-class _MiActividadViewState extends State<MiActividadView>{
-  Widget listaPaquetes;
-  Widget listaViajes;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Mi Actividad'),
-      ),
-      backgroundColor: Colors.white,
-      body: ActividadDetails(widget: widget)
-    );
-  }
-}
-
-class ActividadDetails extends StatefulWidget {
-  const ActividadDetails({
-    Key key,
-    @required this.widget,
-  }) : super(key:key);
-
-  final MiActividadView widget;
-
-  @override
-  _ActividadDetailsState createState() => _ActividadDetailsState();
-}
-
-class _ActividadDetailsState extends State<ActividadDetails> {
+class _MiActividadViewState extends State<MiActividadView> {
   Map<String, bool> _categoryExpansionStateMap = Map<String, bool>();
   bool isExpanded;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     _categoryExpansionStateMap = {
-      "Publicados":false,
-      "En Curso":false,
-      "Finalizados":false,
+      "Publicados": false,
+      "En Curso": false,
+      "Finalizados": false,
     };
 
     isExpanded = false;
@@ -58,68 +33,80 @@ class _ActividadDetailsState extends State<ActividadDetails> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        padding: EdgeInsets.all(10.0),
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: <Widget>[
-            ExpansionPanelList(
-              expansionCallback: (int index, bool isExpanded) {
-                setState(() {
-                  _categoryExpansionStateMap[_categoryExpansionStateMap.keys.toList()[index]] = !isExpanded;
-                });
-              },
-              children: <ExpansionPanel>[
-                ExpansionPanel(
-                  headerBuilder: (BuildContext context, bool isExpanded){
-                    return ListTile(
-                      title: Text(
-                        "Publicados",
-                        style:TextStyle(fontWeight: FontWeight.w500)
-                      )
-                    );
-                  },
-                    body: Publicados(),
-                    isExpanded: _categoryExpansionStateMap["Publicados"]
-                ),
-                ExpansionPanel(
-                  headerBuilder: (BuildContext context, bool isExpanded){
-                    return ListTile(
-                      title: Text(
-                        "En Curso",
-                        style:TextStyle(fontWeight: FontWeight.w500)
-                      )
-                    );
-                  },
-                    body: Publicados(),
-                    isExpanded: _categoryExpansionStateMap["En Curso"]
-                ),
-                ExpansionPanel(
-                  headerBuilder: (BuildContext context, bool isExpanded){
-                    return ListTile(
-                      title: Text(
-                        "Finalizados",
-                        style:TextStyle(fontWeight: FontWeight.w500)
-                      )
-                    );
-                  },
-                    body: Publicados(),
-                    isExpanded: _categoryExpansionStateMap["Finalizados"]
-                ),
-              ],
-            )
-          ],
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Mi Actividad'),
+          backgroundColor: TransportifyColors.primarySwatch,
         ),
-      ),
-    );
-  }
-}
-
-class Publicados extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    
-    return null;
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: <Widget>[
+                ExpansionPanelList(
+                  expansionCallback: (int index, bool isExpanded) {
+                    setState(() {
+                      _categoryExpansionStateMap[_categoryExpansionStateMap.keys
+                          .toList()[index]] = !isExpanded;
+                    });
+                  },
+                  children: <ExpansionPanel>[
+                    ExpansionPanel(
+                        canTapOnHeader: true,
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                            leading: Icon(
+                              Icons.new_releases,
+                              color: TransportifyColors.primarySwatch,
+                            ),
+                            title: Text("Publicados",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 16,
+                                    color: TransportifyColors.primarySwatch)),
+                          );
+                        },
+                        body: MiActividadBusqueda(
+                            EstadoActividad.PUBLICADO, widget.usuario),
+                        isExpanded: _categoryExpansionStateMap["Publicados"]),
+                    ExpansionPanel(
+                        canTapOnHeader: true,
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                              leading: Icon(
+                                Icons.sync,
+                                color: Colors.pink,
+                              ),
+                              title: Text("En Curso",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 16,
+                                      color: Colors.pink)));
+                        },
+                        body: MiActividadBusqueda(
+                            EstadoActividad.PUBLICADO, widget.usuario),
+                        isExpanded: _categoryExpansionStateMap["En Curso"]),
+                    ExpansionPanel(
+                        canTapOnHeader: true,
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                              leading: Icon(Icons.check_box,
+                                  color: Colors.grey[500]),
+                              title: Text("Finalizados",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 16,
+                                      color: Colors.grey[500])));
+                        },
+                        body: MiActividadBusqueda(
+                            EstadoActividad.PUBLICADO, widget.usuario),
+                        isExpanded: _categoryExpansionStateMap["Finalizados"]),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
