07880be3e1682099a3b563160c1c49f3db840e1f
Nacchofer31 <ignaciofer31@gmail.com>
2019-12-09 01:24:51 +0100

    Changed titles and sizes

--- lib/middleware/ActividadBD.dart
+++ lib/middleware/ActividadBD.dart
@@ -8,112 +8,165 @@ import 'package:transportify/modelos/Usuario.dart';
 import '../modelos/Paquete.dart';
 import '../modelos/Viaje.dart';
 
-class ActividadBD{
-
+class ActividadBD {
   static const String coleccion_viajes = 'viajes';
   static const String coleccion_paquetes = 'paquetes';
 
   static StreamBuilder<Map<Type, QuerySnapshot>> obtenerStreamBuilderListado(
-    Function(BuildContext,
-    AsyncSnapshot<Map<Type, QuerySnapshot>>) builder,
-    MultipleCollectionStreamSystem multipleCollectionStreamSystem) =>
-      Datos.obtenerStreamBuilderFromMultipleCollectionStreamSystem(multipleCollectionStreamSystem,builder);
-  
+          Function(BuildContext, AsyncSnapshot<Map<Type, QuerySnapshot>>)
+              builder,
+          MultipleCollectionStreamSystem multipleCollectionStreamSystem) =>
+      Datos.obtenerStreamBuilderFromMultipleCollectionStreamSystem(
+          multipleCollectionStreamSystem, builder);
+
   static Widget obtenerListaActividadesCards(
-    {Usuario usuario,@required MultipleCollectionStreamSystem multipleCollectionStreamSystem}){
-    return obtenerStreamBuilderListado(
-      _obtenerListaActividadesBuilder(usuario),multipleCollectionStreamSystem);
+      {Usuario usuario,
+      @required
+          MultipleCollectionStreamSystem multipleCollectionStreamSystem}) {
+    return obtenerStreamBuilderListado(_obtenerListaActividadesBuilder(usuario),
+        multipleCollectionStreamSystem);
   }
 
   static Function(BuildContext, AsyncSnapshot<Map<Type, QuerySnapshot>>)
-    _obtenerListaActividadesBuilder(
-        Usuario usuario) {
-          return (BuildContext context, AsyncSnapshot<Map<Type, QuerySnapshot>> snapshot){
-            return _obtenerListaActividadesCards(context, snapshot);
-          };
-        }
+      _obtenerListaActividadesBuilder(Usuario usuario) {
+    return (BuildContext context,
+        AsyncSnapshot<Map<Type, QuerySnapshot>> snapshot) {
+      return _obtenerListaActividadesCards(context, snapshot, usuario);
+    };
+  }
 
-  static Widget _obtenerListaActividadesCards(
-    BuildContext context, 
-    AsyncSnapshot<Map<Type, QuerySnapshot>> snapshot) {
+  static Widget _obtenerListaActividadesCards(BuildContext context,
+      AsyncSnapshot<Map<Type, QuerySnapshot>> snapshot, Usuario usuario) {
     if (!snapshot.hasData)
       return const Center(child: const CircularProgressIndicator());
 
-    //var listaPaquetes = snapshot.data[Paquete];
-    //var listaViajes = snapshot.data[Viaje];
+    var paquetes = snapshot.data[Paquete].documents
+        .map((snapshot) => Paquete.fromSnapshot(snapshot))
+        .where((paquete) =>
+            paquete.viajeAsignado == null &&
+            (usuario == null || paquete.remitente == usuario));
+
+    var viajes = snapshot.data[Viaje].documents
+        .map((snapshot) => Viaje.fromSnapshot(snapshot))
+        .where((viaje) => (usuario == null || viaje.transportista == usuario));
 
     return ListView.builder(itemBuilder: (context, index) {
-      
+      if (index >= 0 && index < viajes.length) {
+        return obtenerCardPaquete(paquetes.elementAt(index));
+      } else if (index >= 0 && index < paquetes.length) {
+        return obtenerCardViaje(viajes.elementAt(index));
+      } else {
+        return null;
+      }
     });
   }
 
-  static Widget _obtenerCardPaquete(DocumentSnapshot snapshot) {
-    Paquete paquete = Paquete.fromSnapshot(snapshot);
-
+  static Widget obtenerCardPaquete(Paquete paquete) {
     return Card(
+      color: Colors.grey[100],
       elevation: 5,
       child: Row(
         children: <Widget>[
           Container(
-            height: 100.0,
+            height: 150.0,
             width: 70.0,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(5),
                     topLeft: Radius.circular(5)),
                 image: DecorationImage(
-                    fit: BoxFit.cover,
+                    fit: BoxFit.contain,
                     image: NetworkImage(
-                        "https://www.shareicon.net/data/512x512/2015/09/30/648924_boxes_512x512.png"))),
+                        "https://image.flaticon.com/icons/png/128/679/679720.png"))),
           ),
-          Container(
+          Flexible(
+              child: Container(
             height: 100,
             child: Padding(
               padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(paquete.nombre),
+                  Text(
+                    paquete.nombre,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Row(children: <Widget>[
+                    Icon(Icons.location_on,color: Colors.grey[500],),
+                    Text(paquete.origen.direccion,style: TextStyle(color: Colors.grey[500],fontStyle:FontStyle.italic),)
+                    ],),
+                  Icon(Icons.keyboard_arrow_down,color: Colors.grey[500]),
+                  Row(children: <Widget>[
+                    Text(paquete.destino.direccion,style: TextStyle(color: Colors.grey[500],fontStyle:FontStyle.italic),),
+                    Icon(Icons.location_on,color: Colors.grey[500],),
+                    ],),
                 ],
               ),
             ),
-          )
+          ))
         ],
       ),
     );
   }
 
-  static Widget _obtenerCardViaje(DocumentSnapshot snapshot) {
-    Viaje viaje = Viaje.fromSnapshot(snapshot);
-
+  static Widget obtenerCardViaje(Viaje viaje) {
     return Card(
+      color: Colors.grey[100],
       elevation: 5,
       child: Row(
         children: <Widget>[
           Container(
-            height: 100.0,
+            height: 150.0,
             width: 70.0,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(5),
                     topLeft: Radius.circular(5)),
                 image: DecorationImage(
-                    fit: BoxFit.cover,
+                    fit: BoxFit.contain,
                     image: NetworkImage(
-                        "https://www.shareicon.net/data/512x512/2015/09/30/648924_boxes_512x512.png"))),
+                        "https://image.flaticon.com/icons/png/128/664/664468.png"))),
           ),
-          Container(
+          Flexible(
+              child: Container(
             height: 100,
             child: Padding(
               padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text("Viaje a "+viaje.destino+" del "+viaje.fecha.toString()+" a las "+viaje.fecha.hour.toString()+":"+viaje.fecha.minute.toString()),
+                  Text("Viaje a " +
+                      viaje.destino +
+                      " del " +
+                      viaje.fecha.day.toString() +
+                      "/" +
+                      viaje.fecha.month.toString() +
+                      " a las " +
+                      viaje.fecha.hour.toString() +
+                      ":" +
+                      viaje.fecha.minute.toString(),
+                      overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),),
+                  Row(children: <Widget>[
+                    Icon(Icons.location_on,color: Colors.grey[500],),
+                    Text(viaje.origen,style: TextStyle(color: Colors.grey[500],fontStyle:FontStyle.italic),)
+                    ],),
+                  Icon(Icons.keyboard_arrow_down,color: Colors.grey[500]),
+                  Row(children: <Widget>[
+                    Text(viaje.destino,style: TextStyle(color: Colors.grey[500],fontStyle:FontStyle.italic),),
+                    Icon(Icons.location_on,color: Colors.grey[500],),
+                    ],),
                 ],
               ),
             ),
-          )
+          ))
         ],
       ),
     );
